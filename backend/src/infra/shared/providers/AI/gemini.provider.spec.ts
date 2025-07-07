ada4366089f87aa11d886711e4f6b971e2ec25a2
Samuel Cristo da Fonseca <samfonsa12345@gmail.com>
2025-07-07 13:25:55 -0400

    test(ai): add gemini tests

---
+++ backend/src/infra/shared/providers/AI/gemini.provider.spec.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+
+describe("Gemini Provider tests", () => {
+    beforeAll(() => {
+        config();
+    });
+    
+    it("should be able to generate a summary of a text with maximum 30 words", async () => {
+        const summary = await new GeminiProvider().generateSummaryByText(
+            "The discovery of photosynthesis in the 18th century revolutionized our understanding of plant biology. Scientists found that plants convert sunlight into chemical energy, releasing oxygen as a byproduct. This process not only sustains plant life but also produces most of Earth's atmospheric oxygen, making it vital for all aerobic organisms. Modern research continues to explore ways to enhance photosynthetic efficiency, potentially boosting agricultural yields and addressing food security challenges"
+        );
+
+        expect(summary.split(" ").length).toBeLessThanOrEqual(30);
+    });
+})
